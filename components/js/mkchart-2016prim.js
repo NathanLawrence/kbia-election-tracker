3b23c0407333123e0fbfffaf7ad96a0f43b27404
Nathan Lawrence <lawrencen@missouri.edu>
2016-03-15 14:25:06 -0500

    I think it's pretty good!

--- components/js/mkchart-2016prim.js
+++ components/js/mkchart-2016prim.js
@@ -7,15 +7,50 @@ var gopdata = {
             strokeColor: "rgba(0, 38, 86, 0.8)",
             highlightFill: "rgba(0, 38, 86, 0.75)",
             highlightStroke: "rgba(0, 38, 86, 1)",
-            data: [65, 59, 80, 81]
+            data: [0, 0, 0, 0]
+        }]
+};
+
+var demdata = {
+    labels: ["Clinton", "Sanders"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(0, 38, 86, 0.5)",
+            strokeColor: "rgba(0, 38, 86, 0.8)",
+            highlightFill: "rgba(0, 38, 86, 0.75)",
+            highlightStroke: "rgba(0, 38, 86, 1)",
+            data: [0, 0]
+        }]
+};
+
+var propdata = {
+    labels: ["Yes", "No"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(0, 38, 86, 0.5)",
+            strokeColor: "rgba(0, 38, 86, 0.8)",
+            highlightFill: "rgba(0, 38, 86, 0.75)",
+            highlightStroke: "rgba(0, 38, 86, 1)",
+            data: [0, 0]
         }]
 };
 
 var gopnom = document.getElementById("gop-nom").getContext("2d");
-
+var demnom = document.getElementById("dem-nom").getContext("2d");
+var prop = document.getElementById("prop").getContext("2d");
 
 Chart.defaults.global.responsive = true;
 
 new Chart(gopnom).Bar(gopdata, {
+    barShowStroke: false
+});
+
+new Chart(demnom).Bar(demdata, {
+    barShowStroke: false
+});
+
+new Chart(prop).Bar(propdata, {
     barShowStroke: false
 });
