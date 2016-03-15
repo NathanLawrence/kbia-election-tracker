fb232dd25a5c61052f7dbe654e70662e655ff634
Nathan Lawrence <lawrencen@missouri.edu>
2016-03-15 13:32:18 -0500

    Renamed files and rebuilt.

--- components/js/graph-mar2016.js
+++ components/js/graph-mar2016.js
@@ -1,13 +1,13 @@
-var data = {
+var gopData = {
 	labels: ["Trump", "Rubio", "Cruz", "Kasich"],
-	datasets: [
+	gopDatasets: [
 		{
-			label: "Dataset 1",
+			label: "gopDataset 1",
 			fillColor: "rgba(0,53,121,0.5)",
             strokeColor: "rgba(0,53,121,0.5)",
             highlightFill: "rgba(0,53,121,0.75)",
             highlightStroke: "rgba(0,53,121,1)",
-            data: [4389, 54149, 11413, 15000]
+            gopData: [4389, 54149, 11413, 15000]
 		}
 	]
 };
@@ -17,7 +17,7 @@ var gopNom = document.getElementById("gop-nom").getContext("2d");
 
 Chart.defaults.global.responsive = true;
 
-new Chart(gopNom).Bar(data, {
+new Chart(gopNom).Bar(gopData, {
 	barShowStroke: false,
 	scaleShowGridLines: true
 });
