f5390ba22a06064029e5c2a23ede229a0bee7d09
Nathan Lawrence <lawrencen@missouri.edu>
2016-03-15 13:29:52 -0500

    Replace premature birth data.

--- components/js/graph-expense.js
+++ components/js/graph-expense.js
@@ -1,22 +1,23 @@
-var data = {
-	labels: ["No Complications", "Premature / Low Birth Weight", "All Births"],
-	datasets: [
+var gopData = {
+	labels: ["Trump", "Rubio", "Cruz", "Kasich"],
+	gopDatasets: [
 		{
-			label: "Dataset 1",
+			label: "gopDataset 1",
 			fillColor: "rgba(0,53,121,0.5)",
             strokeColor: "rgba(0,53,121,0.5)",
             highlightFill: "rgba(0,53,121,0.75)",
             highlightStroke: "rgba(0,53,121,1)",
-            data: [4389, 54149, 11413]
+            gopData: [4389, 54149, 11413, 15000]
 		}
 	]
 };
 
-var ctx = document.getElementById("myChart").getContext("2d");
+var gopNom = document.getElementById("gop-nom").getContext("2d");
+
 
 Chart.defaults.global.responsive = true;
 
-new Chart(ctx).Bar(data, {
+new Chart(gopNom).Bar(gopData, {
 	barShowStroke: false,
 	scaleShowGridLines: true
 });
